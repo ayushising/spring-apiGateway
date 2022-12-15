785acf85675eecf012f2ed2e5523d85eab5718b6
Ayushi_Singh <66073819+ayushising@users.noreply.github.com>
2022-12-15 15:59:21 +0530

    Add files via upload

---
+++ productservice/src/main/java/com/globallogic/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.globallogic.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.globallogic.modal.Product;
+
+@Repository
+public interface ProductRepository extends MongoRepository<Product,Integer> {
+
+}
